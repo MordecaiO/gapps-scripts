32bffbfa824397726f9776cc17d33d8591268938
MordecaiO <cai.oladimeji@gmail.com>
2023-12-07 18:22:28 +0000

    Update phoneNumberParser.js

--- phoneNumberParser.js
+++ phoneNumberParser.js
@@ -6,3 +6,11 @@ then add +
 */
 
 
+const numberParse = (phoneNum) => {
+    let phoneNumAsString = phoneNum.toString() 
+    if(phoneNumAsString.length === 12 && phoneNumAsString[0] === "4" && phoneNumAsString[1] === "4"){
+        return "+" + phoneNumAsString ; 
+    }
+}
+
+console.log(numberParse(447454480493))
